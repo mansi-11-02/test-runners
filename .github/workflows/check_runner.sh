accb6e8a9cc21531dd58e92acb01ed00fe032e04
mansi-11-02 <mansi.10747874@ltimindtree.com>
2025-06-20 16:35:26 +0530

    Delete .github/workflows directory

--- .github/workflows/check_runner.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Starting self-hosted runner check..."
-echo "Hostname: $(hostname)"
-echo "Date: $(date)"
-echo "Current Directory: $(pwd)"
-echo "User: $(whoami)"
-echo "Self-hosted runner triggered successfully!"
